dee5ec650331bac3225bfbc80c11e4a179f250c6
tjens23 <tjens23@student.sdu.dk>
2025-12-01 15:42:36 +0100

    Commit from PC

---
+++ configuration.nix
@@ -0,0 +1,152 @@
+{ config, lib, pkgs, ... }:
+
+{
+  imports =
+    [
+      # Include the results of the hardware scan.
+      ./hardware-configuration.nix
+    ];
+
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+
+  nixpkgs.config.allowUnfree = true;
+  networking.hostName = "sdu";
+  networking.networkmanager.enable = true; # Easiest to use and most distros use this by default.
+  services.displayManager.ly.enable = true;
+
+  time.timeZone = "Europe/Copenhagen";
+
+  services.xserver = {
+    enable = true;
+    autoRepeatDelay = 200;
+    autoRepeatInterval = 35;
+    windowManager.qtile.enable = true;
+
+    # Use NVIDIA proprietary drivers
+    videoDrivers = [ "nvidia" ];
+
+    displayManager.sessionCommands = ''
+      xrandr --output HDMI-1 --mode 1920x1080 --rate 144 --primary
+      xwallpaper --zoom ~/dotfiles/walls/wall1.png
+    '';
+  };
+
+  programs.steam = {
+    enable = true;
+    remotePlay.openFirewall = true;
+    dedicatedServer.openFirewall = true;
+    gamescopeSession.enable = true;
+
+  };
+  # NVIDIA configuration
+  hardware.nvidia = {
+    package = config.boot.kernelPackages.nvidiaPackages.beta;
+    modesetting.enable = true;
+    powerManagement.enable = true;
+    open = false;
+    nvidiaSettings = true;
+  };
+
+  # Updated: hardware.opengl is now hardware.graphics
+  hardware.graphics = {
+    enable = true;
+    enable32Bit = true; # For 32-bit apps/games
+  };
+
+
+  programs.alvr.openFirewall = true;
+  programs.dconf.enable = true;
+  services.picom.enable = true;
+  services.xserver.xkb.layout = "dk";
+
+  services.postgresql = {
+    enable = true;
+    ensureDatabases = [ "postgres" ];
+  };
+
+  services = {
+    asusd = {
+      enable = true;
+      enableUserService = true;
+    };
+  };
+
+
+  security.rtkit.enable = true; # Real-time scheduling for low latency
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
+    wireplumber.enable = true;
+    jack.enable = false;
+
+    # Add audio configuration here
+    extraConfig.pipewire."92-low-latency" = {
+      context.properties = {
+        default.clock.rate = 48000;
+        default.clock.quantum = 128;
+        default.clock.min-quantum = 32;
+        default.clock.max-quantum = 512;
+      };
+    };
+  };
+
+  programs.wireshark.enable = true;
+
+  users.users.toby = {
+    isNormalUser = true;
+    extraGroups = [ "wheel" "docker" "audio" "wireshark" "input" "plugdev" ];
+    packages = with pkgs; [
+      tree
+    ];
+  };
+
+
+
+  programs.firefox.enable = true;
+
+  environment.systemPackages = with pkgs; [
+    vim
+    git
+    arandr
+    wget
+    curl
+    alacritty
+  ];
+
+  fonts.packages = with pkgs; [
+    nerd-fonts.jetbrains-mono
+  ];
+
+  virtualisation.docker.enable = true;
+
+  services.twingate.enable = true;
+
+  programs.nh = {
+    enable = true;
+    clean.enable = true;
+    clean.extraArgs = "--keep-since 4d --keep 3";
+    flake = "/home/toby/dotfiles/";
+  };
+
+  # ... your existing config ...
+
+  fileSystems."/mnt/development" = {
+    device = "/dev/disk/by-uuid/8a3c73d1-8cf6-4c19-9653-4677fe81d56e";
+    fsType = "ext4";
+    options = [ "nofail" ];
+  };
+
+  fileSystems."/mnt/games" = {
+    device = "/dev/disk/by-uuid/5419c59c-ff56-4c37-a1e7-196c21593cdb";
+    fsType = "ext4";
+    options = [ "nofail" ];
+  };
+
+  nix.settings.experimental-features = [ "nix-command" "flakes" ];
+  system.stateVersion = "25.11";
+
+}
+
---
+++ home.nix
@@ -0,0 +1,100 @@
+{ config, pkgs, ... }:
+
+let
+  dotfiles = "${config.home.homeDirectory}/dotfiles/config";
+  create_symlink = path: config.lib.file.mkOutOfStoreSymlink path;
+
+  # Standard .config/directory
+  configs = {
+    qtile = "qtile";
+    nvim = "nvim";
+    rofi = "rofi";
+    alacritty = "alacritty";
+    picom = "picom";
+  };
+in
+
+{
+  nixpkgs.config.allowUnfree = true;
+  home.username = "toby";
+  home.homeDirectory = "/home/toby";
+  programs.git = {
+    enable = true;
+    userName = "tjens23";
+    userEmail = "tjens23@student.sdu.dk";
+    extraConfig = {
+      init.defaultBranch = "main";
+    };
+  };
+  home.stateVersion = "25.11";
+  programs.bash = {
+    enable = true;
+    shellAliases = {
+      btw = "echo i use nixos-btw";
+      nrs = "sudo nixos-rebuild switch --flake ~/dotfiles#sdu";
+    };
+    initExtra = ''
+      	  export PS1="\[\e[38;5;75m\]\u@\h \[\e[38;5;113m\]\w \[\e[38;5;189m\]\$ \[\e[0m\]"
+      	'';
+  };
+
+  xdg.configFile = builtins.mapAttrs
+    (name: subpath: {
+      source = create_symlink "${dotfiles}/${subpath}";
+      recursive = true;
+    })
+    configs;
+
+  services.easyeffects.enable = true;
+  home.packages = with pkgs; [
+    postgresql_17_jit
+    easyeffects
+    obs-studio
+    kubectl
+    xournalpp
+    alvr
+    xf86_input_wacom
+    twingate
+    tmux
+    google-cloud-sdk
+    speedcrunch
+    go
+    prismlauncher
+    tshark
+    twingate
+    act
+    sageWithDoc
+    vlc
+    vscode
+    steam
+    sidequest
+    flameshot
+    kdePackages.okular
+    protonplus
+    unzip
+    i3lock-color
+    jetbrains-toolbox
+    dysk
+    jq
+    spotify
+    neovim
+    pay-respects
+    fastfetch
+    google-cloud-sdk
+    jdk25_headless
+    gemini-cli
+    vesktop
+    ripgrep
+    nil
+    usbutils
+    nixpkgs-fmt
+    nodejs
+    fzf
+    yazi
+    gcc
+    rofi
+    xwallpaper
+  ];
+
+}
+
