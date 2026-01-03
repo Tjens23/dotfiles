bb122c00ebe7fbc2706fefda963aa56dee94e512
tjens23 <tjens23@student.sdu.dk>
2026-01-03 16:18:58 +0100

    eslint

--- config/nvim/lua/plugins/linting.lua
+++ config/nvim/lua/plugins/linting.lua
@@ -7,10 +7,10 @@ return {
 
             lint.linters_by_ft = {
                 python = { "pylint" },
-                javascript = { "eslint_d" },
-                typescript = { "eslint_d" },
-                javascriptreact = { "eslint_d" },
-                typescriptreact = { "eslint_d" },
+                javascript = { "eslint" },
+                typescript = { "eslint" },
+                javascriptreact = { "eslint" },
+                typescriptreact = { "eslint" },
                 go = { "golangcilint" },
                 c = { "cpplint" },
                 cpp = { "cpplint" },
