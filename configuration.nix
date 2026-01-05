4329b482c6082db1b9fe43a767535e16c2a4c665
tjens23 <tjens23@student.sdu.dk>
2026-01-05 17:47:09 +0100

    feat: Cuda for ollama

--- configuration.nix
+++ configuration.nix
@@ -61,6 +61,11 @@
 
   services.ollama = {
     enable = true;
+    acceleration = "cuda";
+    environmentVariables = {
+      OLLAMA_FLASH_ATTENTION = "1";
+      OLLAMA_KV_CACHE_TYPE = "q8_0"; # Reduces context RAM usage by 50%
+    };
   };
 
   hardware.bluetooth = {
